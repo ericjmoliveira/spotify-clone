11b934925068f8e3356082dbf87a6ab518b8f50d
Eric Oliveira <ericjmoliveira@gmail.com>
2022-04-07 10:04:19 -0300

    Change font sizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ const menuButtonTopBar = document.querySelector('.button--top-bar');
 const menuButtonMiddleBar = document.querySelector('.button--middle-bar');
 const menuButtonBottomBar = document.querySelector('.button--bottom-bar');
 const navbar = document.querySelector('.header__navbar');
-const navLinks = document.querySelectorAll('.navbar__list li a');
 
 menuButton.addEventListener('click', () => {
     menuButtonTopBar.classList.toggle('top-bar--active');
