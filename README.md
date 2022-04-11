02657b3ee3570a1842e8463d5dd519e7d85f1321
Eric Oliveira <ericjmoliveira@gmail.com>
2022-04-11 10:28:23 -0300

    Update media queries

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spotify Clone
+
+Mobile-first responsive Spotify's homepage clone using HTML, CSS and JavaScript, for studying purposes only.
+
+Link: https://ericjmoliveira.github.io/spotify-clone
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,14 @@ const menuButtonMiddleBar = document.querySelector('.button--middle-bar');
 const menuButtonBottomBar = document.querySelector('.button--bottom-bar');
 const navbar = document.querySelector('.header__navbar');
 
+// Handles the transition on the mobile menu
+window.onresize = () => {
+    window.innerWidth < 992
+        ? navbar.classList.add('navbar--mobile')
+        : navbar.classList.remove('navbar--mobile');
+};
+
+// Handles the mobile menu
 menuButton.addEventListener('click', () => {
     menuButtonTopBar.classList.toggle('top-bar--active');
     menuButtonMiddleBar.classList.toggle('middle-bar--active');
