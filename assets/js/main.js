0f34fe2caeede4e4c6c2e5f46ff8449443a9627f
Eric Oliveira <ericjmoliveira@gmail.com>
2022-08-05 10:04:09 -0300

    Adjust navbar transition

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,12 @@ menuButton.addEventListener('click', () => {
     menuButtonTopBar.classList.toggle('top-bar--active');
     menuButtonMiddleBar.classList.toggle('middle-bar--active');
     menuButtonBottomBar.classList.toggle('bottom-bar--active');
-    navbar.classList.toggle('navbar--active');
+    navbar.classList.toggle('navbar--enabled');
+    navbar.classList.toggle('navbar--disabled');
     document.body.classList.toggle('body--fixed');
 
     links.forEach((link, index) => {
-        if (navbar.classList.contains('navbar--active')) {
+        if (navbar.classList.contains('navbar--enabled')) {
             link.style.animation = `animateLinks .5s ${0.025 * index}s`;
         } else link.style.animation = '';
     });
